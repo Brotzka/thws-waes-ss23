42541c3acf22dfc38c12c19cf25482b5f733a443
Fabian Hagen <fabianhagen87@gmail.com>
2023-06-16 09:33:31 +0000

    Test

---
+++ frontend/src/components/CheckList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onMounted, ref} from 'vue'
+import axios from 'axios'
+
+let checkLists = []
+let foo = "bar"
+
+defineProps({
+    title: String
+})
+
+onMounted(() => {
+    console.log("Test")
+    console.log("Checklists", checkLists)
+
+    axios.get('http://localhost:80/api/check-lists')
+    .then(r => {
+        console.log(r)
+        checkLists = r.data.data.data
+    })
+    .catch(e => {
+        console.warn(e.message)
+    })
+    console.log("Checklists neu", checkLists)
+})
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
